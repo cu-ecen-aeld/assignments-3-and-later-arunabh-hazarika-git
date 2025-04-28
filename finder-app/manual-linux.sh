80a1d1a564c5010eaa245a6a93a586e5fd380788
Arunabh Hazarika <arunabh.hazarika@gmail.com>
2025-04-28 00:28:55 -0700

    Submitting

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -153,7 +153,8 @@ make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
 # on the target rootfs
 cp finder.sh ${OUTDIR}/rootfs/home/
 cp finder-test.sh ${OUTDIR}/rootfs/home/
-cp writer ${OUTDIR}/rootfs/home
+cp autorun-qemu.sh ${OUTDIR}/rootfs/home/
+cp writer ${OUTDIR}/rootfs/home/
 if [ ! -d "${OUTDIR}/rootfs/home/conf" ];
 then
     mkdir ${OUTDIR}/rootfs/home/conf
